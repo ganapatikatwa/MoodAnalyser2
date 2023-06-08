53f204b8cb92b65045c78f58710f7355508072a4
Ganapati <ganapatasakatwa@gmail.com>
2023-06-08 05:48:23 +0530

    [GK] Return Happy or Sad Mood

---
+++ MoodAnalyser/MoodAnalyse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyse
+    {
+        public string HAPPYMOOD = "Happy";
+        public string SADMOOD = "Sad";
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyse moodobj = new MoodAnalyse();
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+
+            Console.WriteLine(moodobj.AnalyseMood(message));
+        }
+    }
+}
