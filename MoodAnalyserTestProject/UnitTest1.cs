2ab7eed65405367c27ed47b424a5bac448372189
Ganapati <ganapatasakatwa@gmail.com>
2023-06-08 06:50:21 +0530

    [GK] Using Constructor Repeat TC1.1

--- MoodAnalyserTestProject/UnitTest1.cs
+++ MoodAnalyserTestProject/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace MoodAnalyserTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenSadMoodReturnSad()
         {
             //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
 
@@ -21,17 +21,6 @@ namespace MoodAnalyserTestProject
             //assert
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void AnyMood()
-        {
-            //Arrange
-            MoodAnalyse obj = new MoodAnalyse();
-            string message = "I am in Any Mood";
-            string expected = "Happy Mood";
-            //act
-            string actual = obj.AnalyseMood(message);
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
+
     }
 }
