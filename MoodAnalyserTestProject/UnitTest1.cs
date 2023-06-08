563d9b6f47d3bd7f2a68fec393babb0c72d82138
Ganapati <ganapatasakatwa@gmail.com>
2023-06-08 06:15:50 +0530

    [GK] In Any mood Return Happy

--- MoodAnalyserTestProject/UnitTest1.cs
+++ MoodAnalyserTestProject/UnitTest1.cs
@@ -13,8 +13,19 @@ namespace MoodAnalyserTestProject
             //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
 
             //Arrange
-            string message = "I am in happy Mood";
             MoodAnalyse obj = new MoodAnalyse();
+            string message = "I am in happy Mood";
+            string expected = "Happy Mood";
+            //act
+            string actual = obj.AnalyseMood(message);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AnyMood()
+        {
+            MoodAnalyse obj = new MoodAnalyse();
+            string message = "I am in Any Mood";
             string expected = "Happy Mood";
             //act
             string actual = obj.AnalyseMood(message);
