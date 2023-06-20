313e8e2889d1be9b732629923861e3b9b6140572
Ganapati <ganapatasakatwa@gmail.com>
2023-06-20 06:28:04 +0530

    [GK] UC5 Reflection Parameter Constructor

--- MoodAnalyser/MoodAnalyserFactory.cs
+++ MoodAnalyser/MoodAnalyserFactory.cs
@@ -1,19 +1,73 @@
-﻿using System;
+﻿using MoodAnalyser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MoodAnalyser1
 {
     public class MoodAnalyserFactory
     {
-        public static object CreateMoodAnalyser(string className)
+        public static object CreateMoodAnalyser(string className, string constructorName)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            Type moodAnalysetype = assembly.GetType(className);
-            return Activator.CreateInstance(moodAnalysetype);
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(pattern, className);
+            try
+            {
+                if (result.Success)
+                {
+                    try
+                    {
+
+                        {
+                            Assembly assembly = Assembly.GetExecutingAssembly();
+                            Type moodAnalysetype = assembly.GetType(className);
+                            return Activator.CreateInstance(moodAnalysetype);
+                        }
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Your Input is not Valid");
+                        throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not Found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not Found");
+                }
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                return e.Message;
+            }
         }
+
+        //UC5
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyse);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not Found");
+
+            }
+        }
+
     }
 }
